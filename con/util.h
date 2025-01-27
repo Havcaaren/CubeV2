686cbc5248432c9bb75fa1f7722ef91aef62c072
Matej Dinis <dinismatej1@gmail.com>
2025-01-27 19:50:36 +0100

    change order

--- con/util.h
+++ con/util.h
@@ -6,16 +6,6 @@
 #include <string.h>
 #include <string.h>
 
-struct line {
-    int number;
-    char *org;
-};
-
-typedef  struct list_node {
-    struct line* node;
-    struct list_node* next;
-} line_list;
-
 char *read_file(const char *fp);
 
 void output_file(const char *fp, size_t len);
@@ -23,20 +13,5 @@ void output_file(const char *fp, size_t len);
 char *remove_comments(char *fp);
 
 char *remove_empty_lines(char *fp);
-
-struct line *create_line(int n, const char *o);
-
-line_list* create_node_list(struct line* l);
-
-void append_line_list(line_list* list, struct line* line);
-
-void free_line(struct line* l);
-
-void free_list(line_list* l);
-
-
-line_list* process_file_to_list(char* f);
-
-
 
 #endif //CUBE_UTIL_H
